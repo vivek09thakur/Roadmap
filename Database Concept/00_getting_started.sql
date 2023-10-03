cf367ebfa1ce6bf7ab6b0cef9ad508646f476141
Vivek Thakur <20509.vivek@gpranchi.org>
2023-10-04 02:25:19 +0530

    commit

--- Database Concept/00_getting_started.sql
+++ Database Concept/00_getting_started.sql
@@ -1,3 +1,8 @@
 -- Getting Started with database concepts and SQL --
+-- Currently I Database Concepts and SQL is far away from my mind. I am just trying to get the basics down because I was
+-- quite excited to know what I am going to learn in the future. So that's why I am just going to write down the basics
+-- and then I will revert back where I left off. --
+
+-- A demo database --
 -- 1. Create a database called 'test' --
 CREATE DATABASE test;
