d4e299c348f56ae7af31a021648e65597139e916
Vivek Thakur <20509.vivek@gpranchi.org>
2024-07-16 14:23:24 +0530

    commit

---
+++ RoadmapJs/Base/01.js
@@ -0,0 +1,17 @@
+// Understanding the print statement
+// console.log("Hello World"); // this is how we can print something in the console
+// console.log("This is a number: ", 10); // we can print multiple things in the console
+// console.log("Hello\nWorld"); // we can use escape characters like \n to print new lines
+// console.log("Hello!\tWorld"); // we can use escape characters like \t to print tabs")
+// console.log("Hello World!\nThis is a number: ", 10, "\nHello!\tWorld");
+// console.log(`
+//     This 
+//     is 
+//     a 
+//     multi-line
+//     long 
+//     string
+// `)
+
+console.log('And\n I\'ve\n done with the first lesson!');
+
