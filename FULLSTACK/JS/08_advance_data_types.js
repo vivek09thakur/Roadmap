21437b540bd81bf32352fa30c5919bd93b88a2b4
Vivek Thakur <20509.vivek@gpranchi.org>
2024-07-18 00:14:10 +0530

    commit

---
+++ FULLSTACK/JS/08_advance_data_types.js
@@ -0,0 +1,23 @@
+// DATA TYPES in Javascript: 
+
+// Primitives :
+//      1. String
+//      2. Number
+//      3. Boolean
+//      4. null
+//      5. undefined
+//      6. Symbol
+//      7. BigInt
+
+// Non primitives (References) : Array, Objects, Functions
+
+// JS a Dynamics Typed Language
+
+const myFunction = function(){
+    console.log("hello world")
+}
+const Null = null
+myFunction()
+
+console.log(typeof(myFunction))
+console.log(typeof(Null))
