da71811922cfe90281885dddcb22013373a0b61b
Vivek Thakur <20509.vivek@gpranchi.org>
2024-07-17 17:51:09 +0530

    commit

---
+++ FULLSTACK/JS/04_conversion.js
@@ -0,0 +1,11 @@
+// Type Casting or Type Conversion
+
+let score = "33ab";
+console.log("CASE 1 :", typeof score, `Data = ${score}`);
+
+let valInNum = Number(score);
+console.log("CASE 2 :", typeof valInNum, `Data = ${valInNum}`);
+
+let isLoggedIn = 0;
+let boolisLoggedIn = Boolean(isLoggedIn);
+console.log("CASE 3 :", typeof boolisLoggedIn, `Data = ${boolisLoggedIn}`);
---
+++ FULLSTACK/JS/05_operations.js
@@ -0,0 +1,47 @@
+// Operations in Javascripts
+// 1. Arthematic Operations
+// 2. Comparison Operations
+// 3. Logical Operations
+
+// 1. Arthematic Operations
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// console.log(1+2)
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log("2"+2+2)
+// console.log(2+3+"2")
+
+// console.log(+true)
+
+// 2. Comparison Operations
+// console.log(2>3)
+// console.log(3>2)
+// console.log(3==2)
+// console.log("Vivek"==="Suraj")
+// console.log(2>=4)
+// console.log(2<=3)
+
+// 3. Logical Operations
+// AND (&&)
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && false)
+// console.log(false && true)
+
+// OR (||)
+
+// console.log(true ||true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// NOT (!)
+// console.log(!true)
+// console.log(!false)
