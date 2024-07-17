bbc6e0e7898e56faeacbc49d67d2e110bf471446
Vivek Thakur <20509.vivek@gpranchi.org>
2024-07-17 18:06:16 +0530

    commit

---
+++ FULLSTACK/JS/06_comparision.js
@@ -0,0 +1,8 @@
+// console.log("2">2)
+// console.log("5" == 1)
+// console.log("5" == 5)
+// console.log("5"===5) // same value , same type
+
+console.log(null>0) // null is not greater than 0
+console.log(null==0) // null is not equals to 0
+console.log(null>=0) // null is greater than or equals to 0? WTF?
