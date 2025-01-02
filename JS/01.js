4029d7650a315f52830ed592e63bbfbffb591715
vivek09thakur <vivekthakurcse20509@gmail.com>
2025-01-02 16:01:00 +0530

    commit

---
+++ JS/01.js
@@ -0,0 +1,18 @@
+// Printing a hello world and this is comment btw
+console.log("Hello World")
+
+// Variables
+
+// Let, Const and Var
+let app = document.getElementById("app")
+console.log("Element we got = ",app)
+console.log("Text it has = ",app.textContent)
+console.log("Lets change the text of app ")
+
+app.textContent = "Fuck you Bitch"
+
+console.log("App content is changed now")
+
+function changeContent(){
+    
+}
